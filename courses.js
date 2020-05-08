816b3437a9652217215808fff25384cd4b3f9fec
Rk-red <1446055235@qq.com>
2020-05-08 14:46:15 +0800

    css

---
+++ courses.js
@@ -0,0 +1,41 @@
+var mathId = ['00','02'];
+mathId.forEach( (item) => {
+  var setMath = document.getElementById(item);
+  setMath.style.backgroundColor = '#0099CC';
+  setMath.innerHTML = '移动端程序开发（实训3-207）';
+})
+
+
+var physicsId = ['20'];
+physicsId.forEach( (item) => {
+  var setPhysics = document.getElementById(item);
+  setPhysics.style.backgroundColor = '#996699';
+  setPhysics.innerHTML = '智能穿戴（实训3-207）'
+});
+
+
+var injuryId = ['50','51'];
+injuryId.forEach( (item) => {
+  var setInjury = document.getElementById(item);
+  setInjury.style.backgroundColor = '#FFFF00';
+  setInjury.innerHTML = 'ps(实训楼3-207）';
+})
+
+var setMind = document.getElementById('60');
+setMind.style.backgroundColor = '#FFFF00';
+setMind.innerHTML = '就业指导（教学楼1-312）';
+
+var setElectrotechnics = document.getElementById('70');
+setElectrotechnics.style.backgroundColor = '#996699';
+setElectrotechnics.innerHTML = '创新创业 （教学楼1-207）';
+
+
+
+
+
+
+setAssemblyExperiment.style.backgroundColor = a;
+
+var setPsychological = document.getElementById('110');
+setPsychological.innerHTML = '大学生心理健康辅导';
+setPsychological.style.backgroundColor = '#FFFF00';
